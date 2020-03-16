df90f40ea2176339b1f883fdca651add0e2f5543
Megan Penn <penn.meg@gmail.com>
2020-03-15 20:56:10 -0400

    Wipd

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -11,34 +11,57 @@ const Button = styled.button`
   font-size: 24px;
   font-weight: bold;
   background-color: ${props => props.backgroundColor};
+
+  :disabled {
+    color: black;
+  }
 `;
 
+const regex = /([*+/-])/;
+
 function Calculator() {
+  const [displayTotal, setDisplayTotal] = useState('');
   const [total, setTotal] = useState('');
+
+  const buttonClick = (value) => {
+    switch(value) {
+      case '=':
+        determineTotal();
+        break;
+      default:
+        setDisplayTotal(displayTotal + value);
+        setTotal(displayTotal);
+    }
+  };
+
+  const determineTotal = () => {
+    const totalArry = total.split(regex);
+    console.log(totalArry);
+  };
 
   return (
     <div className='calculator'>
-      <CalculatorView total={total} />
+      <CalculatorView total={displayTotal} />
       <div className='buttonContainer'>
-        <Button backgroundColor="#696969">C</Button>
-        <Button backgroundColor="#696969">+/-</Button>
-        <Button backgroundColor="#696969">%</Button>
-        <Button backgroundColor="#FFA500">/</Button>
-        <Button backgroundColor="#808080">7</Button>
-        <Button backgroundColor="#808080">8</Button>
-        <Button backgroundColor="#808080">9</Button>
-        <Button backgroundColor="#FFA500">x</Button>
-        <Button backgroundColor="#808080">4</Button>
-        <Button backgroundColor="#808080">5</Button>
-        <Button backgroundColor="#808080">6</Button>
-        <Button backgroundColor="#FFA500">-</Button>
-        <Button backgroundColor="#808080">1</Button>
-        <Button backgroundColor="#808080">2</Button>
-        <Button backgroundColor="#808080">3</Button>
-        <Button backgroundColor="#FFA500">+</Button>
-        <Button backgroundColor="#808080"className='zero'>0</Button>
-        <Button backgroundColor="#808080">.</Button>
-        <Button backgroundColor="#FFA500">=</Button>
+        <Button onClick={() => buttonClick("Clear")} backgroundColor="#696969">C</Button>
+        <Button disabled backgroundColor="#696969">+/-</Button>
+        <Button disabled backgroundColor="#696969">%</Button>
+        <Button onClick={() => buttonClick("/")} backgroundColor="#FFA500">/</Button>
+        <Button onClick={() => buttonClick("7")} backgroundColor="#808080">7</Button>
+        <Button onClick={() => buttonClick("8")} backgroundColor="#808080">8</Button>
+        <Button onClick={() => buttonClick("9")} backgroundColor="#808080">9</Button>
+        <Button onClick={() => buttonClick("*")} backgroundColor="#FFA500">x</Button>
+        <Button onClick={() => buttonClick("4")} backgroundColor="#808080">4</Button>
+        <Button onClick={() => buttonClick("5")} backgroundColor="#808080">5</Button>
+        <Button onClick={() => buttonClick("6")} backgroundColor="#808080">6</Button>
+        <Button onClick={() => buttonClick("-")} backgroundColor="#FFA500">-</Button>
+        <Button onClick={() => buttonClick("1")} backgroundColor="#808080">1</Button>
+        <Button onClick={() => buttonClick("2")} backgroundColor="#808080">2</Button>
+        <Button onClick={() => buttonClick("3")} backgroundColor="#808080">3</Button>
+        <Button onClick={() => buttonClick("+")} backgroundColor="#FFA500">+</Button>
+        <Button onClick={() => buttonClick("0")} backgroundColor="#808080"className='zero'>0</Button>
+        <Button disabled backgroundColor="#808080">.</Button>
+        <Button onClick={() => buttonClick("=")} backgroundColor="#FFA500">=</Button>
       </div>
     </div>
   );
